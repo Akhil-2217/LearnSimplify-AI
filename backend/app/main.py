ee171f336a5a279564c7be949bab4eaecb5ed5d6
Akhil Pamarthi <akhilpamarthi22@gmail.com>
2026-09-11 09:31:01 +0530

    Modify CORS settings in main.py
    Updated CORS middleware settings to allow specific HTTP methods and disabled credentials.

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,12 +32,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://akhil-2217.github.io",
-],
-    allow_credentials=True,
-    allow_methods=["*"],
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://akhil-2217.github.io",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
